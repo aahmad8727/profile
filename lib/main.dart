880ed869946acc0e5e0d85c07a39fe5a189359be
aahmad8727 <aaminaahmad8155@gmail.com>
2025-04-07 15:52:41 -0400

    edited main and testing'

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,21 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'auth.dart';
+import 'ascree.dart';
 
-class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
 
-  Future<User?> registerWithEmail(String email, String password) async {
-    try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return result.user;
-    } catch (e) {
-      print(e);
-      return null;
-    }
-  }
-
-  Future<User?> signInWithEmail(String email, String password) async {
-    try {
-      UserCredential result = await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return result.user;
-    } catch (e) {
-      print(e);
-      return null;
-    }
-  }
-
-  Future signOut() async {
-    await _auth.signOut();
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase Auth Demo',
+      home: AuthenticationScreen(), // your custom authentication screen
+    );
   }
 }
